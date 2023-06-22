86ef322415112821200b8d095f8f130a40412f51
ceecedy <ceeceddy.pro@gmail.com>
2023-06-22 16:49:02 +0800

    hello world java file
    hello world class file

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+class helloworld {
+
+    public static void main(String[] args) {
+        int a = 1; 
+        int b = 1;
+        
+        System.out.println("\nHello World!\n");
+        System.out.println("1 + 1 = " + (a + b) + "\n");
+    }
+}
