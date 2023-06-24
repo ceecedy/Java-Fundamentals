de9522f225c44e567d1e870df2f1ac8e5bd366ff
ceecedy <ceeceddy.pro@gmail.com>
2023-06-24 16:14:21 +0800

    Scratch for literals java file

---
+++ Scratches/literals.java
@@ -0,0 +1,23 @@
+public class literals {
+
+    public static void main(String[] args) {
+        
+        //literals
+
+        int a = 0b101; // this is byte number equivalent to 5.
+
+        // to command a byte number in code this is the syntax. 
+        // = 0 + [b] + [digits of binaries]
+
+        System.out.println(a);
+
+        int b = 10_000_000; 
+        
+        // This will ignore the underscore.
+        // The underscore will help you to read the values better. 
+
+        System.out.println(b);
+
+    }
+    
+}
