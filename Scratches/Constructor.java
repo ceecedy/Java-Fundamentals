0219f55813add77ed3560ca8b798c5a2af290011
ceecedy <ceeceddy.pro@gmail.com>
2023-06-25 09:10:04 +0800

    I put another example in camel casing

--- Scratches/Constructor.java
+++ Scratches/Constructor.java
@@ -54,5 +54,5 @@ public class Constructor {
 /*
  * On this file, this will demonstrate the method overloading and constructors in oop.
  * Camel casing - it is a declaration on variable name which the casing is up and down.
-        - e.g. brandNameConvention
+        - e.g. brandNameConvention, brand_name_convention
  */
