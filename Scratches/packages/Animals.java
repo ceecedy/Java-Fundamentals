100ec468fbbbea813645ae23b2259bb64f52b3d2
ceecedy <ceeceddy.pro@gmail.com>
2023-06-28 08:53:20 +0800

    Animals java file

---
+++ Scratches/packages/Animals.java
@@ -0,0 +1,35 @@
+/*
+        * This java file is a package included in the packages file. 
+            - This will be used in the Package.java in the Scratches file. 
+ */
+package packages;
+
+public class Animals {
+
+    public static class cat {
+
+        public void catMessage() {
+            System.out.println("I am Cat. I can eat and say 'Meow!'");
+        }
+
+    }
+
+    public static class dog {
+
+        public void dogMessage() {
+            System.out.println("I am Dog. I can eat and say 'Argh!'");
+        }
+    }
+
+    public static class kangaroo {
+
+        public void kangarooMessage() {
+            System.out.println("I am Kangaroo. I can eat and 'PUNCH!'");
+        }
+    }
+}
+
+/*
+        * Nested class
+            - On this program, nested class were used to have different classes inside a top-level class of a file. 
+ */
