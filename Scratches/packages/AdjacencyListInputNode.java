cd39e84eef595c2c42fa42c62ca97c4e1fa9409e
ceecedy <ceeceddy.pro@gmail.com>
2023-08-25 14:28:38 +0800

    Adjacency list input processor class

---
+++ Scratches/packages/AdjacencyListInputNode.java
@@ -0,0 +1,13 @@
+package packages;
+
+public class AdjacencyListInputNode {
+
+    // making the data variable. 
+    char data; 
+
+    // making constructor to store data in variable data. 
+    public AdjacencyListInputNode(char inputDataNode) {
+        this.data = inputDataNode;
+    }
+    
+}
