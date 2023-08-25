802c6b9a52772b1982ab9aa423606de81eb0f3e2
ceecedy <ceeceddy.pro@gmail.com>
2023-08-25 11:33:09 +0800

    Class that accepts node input in graph using adjacency matrix

---
+++ Scratches/packages/AdjacencyMatrixInputNode.java
@@ -0,0 +1,14 @@
+package packages;
+
+public class AdjacencyMatrixInputNode {
+
+    // This class is to have input node possible. 
+
+    char nodeData; 
+
+    // constructor to have input of data in the nodeData
+    public AdjacencyMatrixInputNode(char data) {
+        this.nodeData = data;
+    }
+
+}
